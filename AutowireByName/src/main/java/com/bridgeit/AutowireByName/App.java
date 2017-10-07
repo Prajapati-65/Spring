46993bb758c42a59937b671e73454455dcc60f90
om65praj <om65praj@gmail.com>
2017-10-07 18:47:55 +0530

    commit

---
+++ AutowireByName/src/main/java/com/bridgeit/AutowireByName/App.java
@@ -0,0 +1,16 @@
+package com.bridgeit.AutowireByName;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	AbstractApplicationContext context = new ClassPathXmlApplicationContext("app-config.xml");
+    	 
+        //autowire=byName 
+        Application application = (Application)context.getBean("appli");
+        System.out.println("Application Details : "+application);
+    }
+}
---
+++ AutowireByName/src/main/java/com/bridgeit/AutowireByName/Application.java
@@ -0,0 +1,21 @@
+package com.bridgeit.AutowireByName;
+
+public class Application {
+
+	private ApplicationUser applicationUser;
+
+	public ApplicationUser getApplicationUser() {
+		return applicationUser;
+	}
+
+	public void setApplicationUser(ApplicationUser applicationUser) {
+		this.applicationUser = applicationUser;
+	}
+
+	@Override
+	public String toString() {
+		return "Application [ applicationUser=" + applicationUser + "]";
+	}
+	
+	
+}
